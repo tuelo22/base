a0a26a6aa43c9f8f880535362a5a01bcb8a051b4
Julio Francis Pereira dos Santos Mendonça <julio_fpsm@hotmail.com>
2024-07-16 03:52:46 -0300

    Realizando melhorias no controle de transação e no conexto do banco de dados.

---
+++ Base.Domain/Base/Interfaces/Transactions/IContextBD.cs
@@ -0,0 +1,9 @@
+﻿namespace Base.Domain.Base.Interfaces.Transactions
+{
+    public interface IContextBD
+    {
+        int SaveChanges();
+
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Base.Domain/Base/Transactions/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Base.Domain.Base.Interfaces.Transactions;
+
+namespace Base.Domain.Base.Transactions
+{
+    public class UnitOfWork(IContextBD _context) : IUnitOfWork
+    {
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+    }
+}
