b59e78fbd21d37c44d69b701ca020db104704f62
Julio Francis Pereira dos Santos Mendonça <julio_fpsm@hotmail.com>
2024-07-23 02:19:47 -0300

    Incluindo métodos assincronos.

--- Base.Domain/Base/Interfaces/Repositories/IRepositoryBase.cs
+++ Base.Domain/Base/Interfaces/Repositories/IRepositoryBase.cs
@@ -29,5 +29,29 @@ namespace Base.Domain.Base.Interfaces.Repositories
         void RemoverLista(IEnumerable<TEntidade> entidade);
 
         IEnumerable<TEntidade> AdicionarLista(IEnumerable<TEntidade> entidades);
+
+        Task<IQueryable<TEntidade>> ListarPorAsync(Expression<Func<TEntidade, bool>> where, params Expression<Func<TEntidade, object>>[] includeProperties);
+
+        Task<IQueryable<TEntidade>> ListarEOrdenadosPorAsync<TKey>(Expression<Func<TEntidade, bool>> where, Expression<Func<TEntidade, TKey>> ordem, bool ascendente = true, params Expression<Func<TEntidade, object>>[] includeProperties);
+
+        Task<TEntidade> ObterPorAsync(Func<TEntidade, bool> where, params Expression<Func<TEntidade, object>>[] includeProperties);
+
+        Task<bool> ExisteAsync(Func<TEntidade, bool> where);
+
+        Task<IQueryable<TEntidade>> ListarAsync(params Expression<Func<TEntidade, object>>[] includeProperties);
+
+        Task<IQueryable<TEntidade>> ListarOrdenadosPorAsync<TKey>(Expression<Func<TEntidade, TKey>> ordem, bool ascendente = true, params Expression<Func<TEntidade, object>>[] includeProperties);
+
+        Task<TEntidade> ObterPorIdAsync(TId id, params Expression<Func<TEntidade, object>>[] includeProperties);
+
+        Task<TEntidade> AdicionarAsync(TEntidade entidade);
+
+        Task<TEntidade> EditarAsync(TEntidade entidade);
+
+        void RemoverAsync(TEntidade entidade);
+
+        void RemoverListaAsync(IEnumerable<TEntidade> entidade);
+
+        Task<IEnumerable<TEntidade>> AdicionarListaAsync(IEnumerable<TEntidade> entidades);
     }
 }
